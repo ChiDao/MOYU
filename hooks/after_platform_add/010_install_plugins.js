14c94279bea2a0a38ac374ae77cdc150834c1575
Freedom He <furui.he@gaeamobile-inc.net>
2015-01-17 08:26:17 +0800

    get clients dynamic

---
+++ hooks/after_platform_add/010_install_plugins.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+ 
+//this hook installs all your plugins
+ 
+// add your plugins to this list--either 
+// the identifier, the filesystem location 
+// or the URL
+var pluginlist = [
+    "org.apache.cordova.console",
+    "org.apache.cordova.statusbar",
+    "org.apache.cordova.splashscreen",
+    "com.ionic.keyboard",
+    "org.apache.cordova.inappbrowser",
+    "technology.kulak.cordova.appopen",
+    "https://github.com/ohh2ahh/AppAvailability.git",
+    "https://github.com/EddyVerbruggen/LaunchMyApp-PhoneGap-Plugin.git --variable URL_SCHEME=gamo",
+];
+
+// no need to configure below
+ 
+var fs = require('fs');
+var path = require('path');
+var sys = require('sys')
+var exec = require('child_process').exec;
+var async = require('async');
+ 
+function puts(error, stdout, stderr) {
+    
+}
+
+async.eachSeries(pluginlist, function(plugin, callback){
+    exec("cordova plugins add " + plugin, function(error, stdout, stderr){
+        if (error){
+            console.log(error);
+            sys.puts(stdout);
+            callback(error);
+        }
+        sys.puts(stdout);
+        callback();
+    })
+}, function(error){
+    return false;
+})
+
+//exec("cordova plugins add ", puts);
---
+++ www/js/controllers.AddChannelCtrl.js
@@ -0,0 +1,8 @@
+define(['app', 'services.RestRoute'], function(app)
+{
+	app.controller('AddChannelCtrl', ['$scope', '$stateParams', 'UI', 'RestRoute', '$ionicFrostedDelegate','$ionicScrollDelegate', '$timeout',
+		function($scope, $stateParams, UI, RestRoute, $ionicFrostedDelegate, $ionicScrollDelegate, $timeout) {
+			//ttp://42.120.45.236:8485/all-clients/ios?_last
+		}
+	]);
+});
