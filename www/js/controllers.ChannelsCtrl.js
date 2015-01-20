336064b75db55e73cf76ca25eb9700467be0e73a
Qoli Wong <llqoli@gmail.com>
2015-01-20 17:05:23 +0800

    remove UI Debug

--- www/js/controllers.ChannelsCtrl.js
+++ www/js/controllers.ChannelsCtrl.js
@@ -3,7 +3,7 @@ define(['app', 'services.RestRoute'], function(app)
   app.controller('ChannelsCtrl', ['$scope', '$state', '$stateParams', 'UI', 'RestRoute',
     function($scope, $state, $stateParams, UI, RestRoute) {
 
-    	UI.testModal('modal-new-clip');
+    	// UI.testModal('modal-new-clip');
 
 		RestRoute.getLinkData('/all-clients/ios?_last', $scope, 'channels').then(function(){
 		console.log($scope.channels);
