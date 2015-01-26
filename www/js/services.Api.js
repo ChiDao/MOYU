4bc5d4741f71aa2d80405fd8b793793e1efd7979
Freedom He <furui.he@gaeamobile-inc.net>
2015-01-26 15:50:48 +0800

    Api.jumpTo()

--- www/js/services.Api.js
+++ www/js/services.Api.js
@@ -1,9 +1,15 @@
 define(['app'], function(app)
 {
-  app.factory('Api', ['$state', '$http', function($state, $http) {
+  app.provider('Api', function() {
+
+    this.baseUrl = '';
+    this.setBaseUrl = function(baseUrl){
+      this.baseUrl = baseUrl;
+    }
 
     //定义api, Todo:服务端生成
-    var makeApi = function(apiType, apiName, apiParams, apiParamsDefault){
+    var apis = [];
+    var createApi = function(apiType, apiName, apiParams, apiContext){
       var api = {
         name: apiName,
         apiRegExpMap: apiParams,
@@ -12,7 +18,7 @@ define(['app'], function(app)
       api.api = apiName;
       var apiRegExpStr = '\\/' + apiName;
       _.forEach(apiParams, function(param){
-        apiRegExpStr += '\\/(\\w+)';
+        apiRegExpStr += (apiContext && apiContext[param]?'(?:\\/(\\w+))?':'\\/(\\w+)');
         api.api += '/<%= ' + param + ' %>';
       })
       if (apiType === 'stream'){
@@ -24,87 +30,120 @@ define(['app'], function(app)
       return api;
     }
 
-    var baseUrl = 'http://localhost:8485';
-    var apis = [];
+    var setApiRouteMap = function(apiName, routeMap){
+      if (api = _.find(apis, {name:apiName})){
+        api.routeMap = routeMap;
+      }
+    }
+
     //'user':'$currentUser'
-    apis.push(makeApi('object', 'home', []));
-    apis.push(makeApi('object', 'game', ['game']));
-    apis.push(makeApi('stream', 'clients-by-platform', ['platform']));
-    apis.push(makeApi('stream', 'recent-user-subscriptions', ['user']));
+    apis.push(createApi('object', 'home', []));
+    apis.push(createApi('object', 'me', [], {'_id':'$currentUser'}));
+    apis.push(createApi('object', 'game', ['game']));
+    apis.push(createApi('stream', 'clients-by-platform', ['platform']));
+    apis.push(createApi('stream', 'recent-user-subscriptions', ['user']));
+    apis.push(createApi('stream', 'event-user', ['user'], {'user':'$currentUser'}));
+
+    setApiRouteMap('me', {
+      'default': 'tab.profile'
+    });
+
     console.debug(apis);
 
-    return {
-      baseUrl: baseUrl,
-      apis: apis,
-      parse: function(apiLink){
-        var apiData = {};
-        //匹配路由并获得参数
-        apiData.api = _.find(apis, function(api) {
-          var matches = api.apiRegExp.exec(apiLink);
-          if (matches){
-            matches.shift();
-            // console.debug(api.apiRegExpMap);
-            apiData.params = _.zipObject(api.apiRegExpMap, matches);
-            // console.debug('get link data params: ' + JSON.stringify(apiData.params));
-            console.log(api.apiRegExp);
+    this.$get = function($http, Modal, $state, $stateParams){
+      return {
+        baseUrl: this.baseUrl,
+        apis: apis,
+        parse: function(apiLink){
+          var apiData = {};
+          //匹配路由并获得参数
+          apiData.api = _.find(apis, function(api) {
+            var matches = api.apiRegExp.exec(apiLink);
+            if (matches){
+              matches.shift();
+              // console.debug(api.apiRegExpMap);
+              apiData.params = _.zipObject(api.apiRegExpMap, matches);
+              // console.debug('get link data params: ' + JSON.stringify(apiData.params));
+              console.log(api.apiRegExp);
+            }
+            return matches;
+          });
+          return apiData;
+        },
+        jumpTo: function(apiLink, context){
+          //get api
+          var apiData = this.parse(apiLink);
+          if (!apiData.api){
+            console.debug("Can't find api:", apiLink);
+            return false;
           }
-          return matches;
-        });
-        return apiData;
-      },
-      getData: function(apiLink, options){
-        //get api
-        var apiData = this.parse(apiLink);
-        if (!apiData.api){
-          console.debug("Can't find api:", apiLink);
-          return Thenjs(function(defer){defer(undefined);});
-        }
-
-        //link
-        var newLink = baseUrl + '/' + _.template(apiData.api.api, apiData.params);
-        if (options && options.shouldAddRandom){
-          newLink += (/\?/.test(newLink)? '&':'?') + 'r=' + Math.random();
-        }
-
-        //http config
-        var httpConfig = {
-          method:'GET',
-          url: newLink,
-          withCredentials: true,
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Requested-With': 'XMLHttpRequest',
-            'Authorization': 'Bearer '+ localStorage.getItem('my-xsrf-header'),
+          var state;
+          if (api.routeMap){
+            if (context && context.name && api.routeMap[context.name]){
+              state = api.routeMap[context.name];
+            }else if (api.routeMap){
+              state = api.routeMap['default'];
+            }
           }
-        };
+          $state.go(state, apiData.params);
+        },
+        getData: function(apiLink, scope, scopeDataField, options){
+          //get api
+          var apiData = this.parse(apiLink);
+          if (!apiData.api){
+            console.debug("Can't find api:", apiLink);
+            return Thenjs(function(defer){defer("Can't find api.");});
+          }
 
-        //get data
-        var retData;
-        Thenjs(function(defer){
-          $http({method:'GET',
+          //link
+          var newLink = this.baseUrl + '/' + _.template(apiData.api.api, apiData.params).replace('/?', '?');
+          if (options && options.random){
+            newLink += (/\?/.test(newLink)? '&':'?') + 'r=' + Math.random();
+          }
+          console.debug(newLink);
+
+          //http config
+          var httpConfig = {
+            method:'GET',
             url: newLink,
             withCredentials: true,
             headers: {
               'Content-Type': 'application/json',
-              'X-Requested-With': 'XMLHttpRequest'
+              'X-Requested-With': 'XMLHttpRequest',
+              'Authorization': 'Bearer '+ localStorage.getItem('my-xsrf-header'),
             }
-          }).success(function(data, status, headers, config){
-            switch (apiData.api.apiType){
-              case 'object':
-                retData = data;
-              case 'stream':
-                retData = data.slice?data.slice:[];
-                retData.meta = _.pick(data, function(value, key){
-                  return key !== 'slice';
-                });
-            }
-            // console.debug(retData);
-            defer(undefined, retData);
-          }).error(function(data, status, headers, config){
-            defer(status);
+          };
+
+          //get data
+          var retData;
+          return Thenjs(function(defer){
+            $http({method:'GET',
+              url: newLink,
+              withCredentials: true,
+              headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest'
+              }
+            }).success(function(data, status, headers, config){
+              switch (apiData.api.apiType){
+                case 'object':
+                  retData = data;
+                case 'stream':
+                  retData = data.slice?data.slice:[];
+                  retData.meta = _.pick(data, function(value, key){
+                    return key !== 'slice';
+                  });
+              }
+              console.debug(retData);
+              scope[scopeDataField] = retData;
+              defer(undefined, retData);
+            }).error(function(data, status, headers, config){
+              defer("http error: " + status);
+            });
           });
-        });
-      }
-    };
-  }]);
+        },
+
+      };
+    }; //End of this.$get 
+  });
 });
