c63053a0c7ec933621ffcbd0e39654569ee09de0
Freedom He <furui.he@gaeamobile-inc.net>
2015-03-09 18:11:22 +0800

    cache channels and chats

--- www/js/controllers.ChannelsCtrl.js
+++ www/js/controllers.ChannelsCtrl.js
@@ -5,8 +5,8 @@ define(['app', 'services.Api','services.Modal'], function(app)
     function($scope, $state, $stateParams, UI, Api, Auth, $ionicLoading,
       $ionicFrostedDelegate, $ionicScrollDelegate, $timeout, $q, Modal, DB) {
       
-      //测试缓存
-      // DB.flatSave('documents', 'id', {id: 'xxx', title: 'test'})
+      // //测试缓存
+      // DB.flatSave('documents', 'id', [{id: 'xxx', title: 'test'},{id: 'yyy', title: 'test2'}])
       // .then(function(defer, result){
       //   DB.flatQueryAll('documents')
       //   .then(function(defer, result){
@@ -42,45 +42,49 @@ define(['app', 'services.Api','services.Modal'], function(app)
           }
         });
 
-        $ionicLoading.show();
-        bindChannels.init()
-        .then(function(defer){
-          // pull refresh
-          $scope.doRefresh = function() {
-            bindChannels.refresh().then(function(defer){
-              $scope.$broadcast('scroll.refreshComplete');
-              $scope.hasMore = bindChannels.moreData.length;
-            }, function(defer){
-              $scope.$broadcast('scroll.refreshComplete');
-              $scope.hasMore = bindChannels.moreData.length;
-            })
-          };
+        bindChannels.setDBTable('channels', '_id')
+        .fin(function(){
+          $ionicLoading.show();
+          bindChannels.init()
+          .then(function(defer){
+            // pull refresh
+            $scope.doRefresh = function() {
+              bindChannels.refresh().then(function(defer){
+                $scope.$broadcast('scroll.refreshComplete');
+                $scope.hasMore = bindChannels.moreData.length;
+              }, function(defer){
+                $scope.$broadcast('scroll.refreshComplete');
+                $scope.hasMore = bindChannels.moreData.length;
+              })
+            };
 
-          $scope.loadMore = function() {
-            bindChannels.more().then(function(defer){
-              $scope.$broadcast('scroll.infiniteScrollComplete');
-              $scope.hasMore = bindChannels.moreData.length;
-            }, function(defer){
-              $scope.$broadcast('scroll.infiniteScrollComplete');
-              $scope.hasMore = bindChannels.moreData.length;
-            })
-          };
-          
-          $scope.enterRefresh = function(){
-            $ionicLoading.show();
-            bindChannels.refresh().fin(function(){
-              $ionicLoading.hide();
-            });
-          }
+            $scope.loadMore = function() {
+              bindChannels.more().then(function(defer){
+                $scope.$broadcast('scroll.infiniteScrollComplete');
+                $scope.hasMore = bindChannels.moreData.length;
+              }, function(defer){
+                $scope.$broadcast('scroll.infiniteScrollComplete');
+                $scope.hasMore = bindChannels.moreData.length;
+              })
+            };
+            
+            $scope.enterRefresh = function(){
+              $ionicLoading.show();
+              bindChannels.refresh().fin(function(){
+                $ionicLoading.hide();
+              });
+            }
 
-          $scope.$on("$ionicView.afterEnter", $scope.enterRefresh);
-          defer(undefined);
-        }, function(defer, error){
-          defer(error)
-        })
-        .fin(function(defer){
-          $ionicLoading.hide();
-        })
+            $scope.$on("$ionicView.afterEnter", $scope.enterRefresh);
+            defer(undefined);
+          }, function(defer, error){
+            defer(error)
+          })
+          .fin(function(defer){
+            $ionicLoading.hide();
+          })
+        },3000)
+        
       }
       if (Auth.isLoggedIn()){
         $scope.bindInit();
