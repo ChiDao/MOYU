e7ec3c0d34b9ce4e1d5a52e1ad2cdde93d7d0d94
Freedom He <furui.he@gaeamobile-inc.net>
2015-03-29 09:12:02 +0800

    revert,remove classy

--- www/js/controllers.TabsCtrl.js
+++ www/js/controllers.TabsCtrl.js
@@ -1,63 +1,61 @@
 define(['app', 'services.Api'], function(app)
 {
-  app.classy.controller({
-    name: 'TabsCtrl',
-    inject: ['$scope', '$state', '$timeout', 'ApiEvent', 'Auth', 'Api'],
-    init: function(){
-      this.$scope.Api = this.Api;
-      //处理新增评论事件
-      this.checkedNewEvent();
-      this.ApiEvent.registerByApi('new-comment', function(event){
-        if (this.$state.current.name === 'tab.chat' || this.$state.current.name === 'tab.chats'){
-          this.ApiEvent.updateEventId();
-          this.$scope.badgeContent = '';
-        }else{
-          this.checkedNewEvent();
-        }
-      });
-    },
-    methods: {
-      checkedNewEvent: function(){
-        this.ApiEvent.checkedNewEvent().then(function(defer, hasNewEvent){
-          $scope.badgeContent = hasNewEvent?'+':'';
-        });
-      },
-      homeData: function(tab){
-        // if(!Auth.isLoggedIn()){
-        //   if (tab === "game") return;
-        //   Auth.login(function(){
-        //       $scope.$broadcast('log-in', true);
-        //   });
-        // }else{
-        return this.Auth.currentUser().userData.homeData;
-        // }            
-      },
-      hideTabs: function(){
-        //根据路由判断是否清理badge
-        switch (this.$state.current.name) {
-          case 'tab.chat':
-          this.$scope.badgeContent = '';
-          break;
-          case 'tab.chats':
-          this.$scope.badgeContent = '';
-          break;
-          default:
-        };
-        //根据路由判断是否显示Tabs
-        switch (this.$state.current.name) {
-          case 'tab.channel':
-          return true;
-          case 'tab.chat':
-          return true;
-          case 'tab.channel-chat':
-          return true;
-          case 'tab.profile-chat':
-          return true;
-          default:
-          return false;
-        }
-      }
-    }
-  });
+  app.controller('TabsCtrl', ['$scope', '$state', '$timeout', 'ApiEvent', 'Auth', 'Api',
+    function($scope, $state, $timeout, ApiEvent, Auth, Api) { 
+        
+    	//处理新增评论事件
+    	var checkedNewEvent = function(){
+	    	ApiEvent.checkedNewEvent().then(function(defer, hasNewEvent){
+				$scope.badgeContent = hasNewEvent?'+':'';
+	    	});
+	    };
+	    checkedNewEvent();
+    	ApiEvent.registerByApi('new-comment', function(event){
+    		if ($state.current.name === 'tab.chat' || $state.current.name === 'tab.chats'){
+    			ApiEvent.updateEventId();
+    			$scope.badgeContent = '';
+    		}else{
+	    		checkedNewEvent();
+	    	}
+    	});
 
+        $scope.Api = Api;
+        $scope.homeData = function(tab){
+          // if(!Auth.isLoggedIn()){
+          //   if (tab === "game") return;
+          //   Auth.login(function(){
+          //       $scope.$broadcast('log-in', true);
+          //   });
+          // }else{
+            return Auth.currentUser().userData.homeData;
+          // }            
+        };
+    	
+    	$scope.hideTabs = function(){
+    		//根据路由判断是否清理badge
+    		switch ($state.current.name) {
+    			case 'tab.chat':
+    				$scope.badgeContent = '';
+                    break;
+    			case 'tab.chats':
+    				$scope.badgeContent = '';
+                    break;
+    			default:
+    		}
+    		//根据路由判断是否显示Tabs
+    		switch ($state.current.name) {
+    			case 'tab.channel':
+    				return true;
+    			case 'tab.chat':
+    				return true;
+    			case 'tab.channel-chat':
+    				return true;
+                case 'tab.profile-chat':
+                    return true;
+    			default:
+    				return false;
+    		}
+    	};
+    }
+  ]);
 });
